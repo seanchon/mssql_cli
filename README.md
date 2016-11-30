5835a29c820dc5553332a53e0e5fabcdc701c90b
Sean Chon <schon@gracenote.com>
2016-11-29 16:52:31 -0800

    Add dynamic insert_results() method to make insert statements easier

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A way to run queries and execute commands against an MSSQL database.
 
 ## Code Example
 ```python
+import datetime  # used for an example below
 from mssql_cli import DBRead, DBWrite
 
 read_connection = DBRead(host='some_host', user='some_user', password='some_password', database='some_database')
@@ -11,7 +12,11 @@ write_connection = DBWrite(host='some_host', user='some_user', password='some_pa
 
 results = read_connection.query("SELECT * FROM some_table")
 more_results = write_connection.query("SELECT * FROM some_table")
-write_connection.execute("INSERT INTO some_table VALUES ('some_data', 'some_more_data')")
+
+write_connection.execute("INSERT INTO [some_table] ([name], [phone_number], [time]) VALUES ('Prince', '111-111-1111', '1999-12-31 23:59:59')")
+
+# insert_results() will create an insert statement based off of a table name and kwargs and execute the resulting statement.
+write_connection.insert_results(table='some_table', name='Prince', phone_number='111-111-1111', time=datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
 
 read_connection.close()
 write_connection.close()
--- mssql_cli/mssql_cli.py
+++ mssql_cli/mssql_cli.py
@@ -32,3 +32,50 @@ class DBWrite(DBRead):
     def execute(self, statement):
         self.cursor.execute(statement)
         self.connection.commit()
+
+    # properly format keys for SQL insert
+    def key_list(self, **kwargs):
+        key_list = ""
+
+        for key, value in kwargs.items():
+            if not key_list:
+                key_list = "[%s]" % (key)
+            else:
+                key_list = "%s, [%s]" % (key_list, key)
+
+        return key_list
+
+    # properly format values for SQL insert
+    def value_list(self, **kwargs):
+        value_list = None
+
+        for key, value in kwargs.items():
+            if value_list is None:
+                if value is None:
+                    value_list = "NULL"
+                else:
+                    value_list = "\'%s\'" % (value)
+            else:
+                if value is None:
+                    value_list = "%s, NULL" % (value_list)
+                else:
+                    value_list = "%s, \'%s\'" % (value_list, value)
+
+        return value_list
+
+    def null_to_false(self, value):
+        if value is None:
+            return 0
+        else:
+            return value
+
+    def true_false_to_int(self, value):
+        if value:
+            return 1
+        else:
+            return 0
+
+    def insert_results(self, table, **kwargs):
+        insert_statement = "INSERT INTO [%s] (%s) VALUES (%s)" % (table, self.key_list(**kwargs), self.value_list(**kwargs))
+        self.execute(insert_statement)
+        return insert_statement
